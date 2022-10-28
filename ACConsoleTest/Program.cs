666bac1c25e0c65980ed4f41c4b4d84e270c774b
MrXANA91 <xana_du_91@hotmail.fr>
2022-10-28 19:15:06 +0200

    Initial commit

---
+++ ACConsoleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using assettocorsasharedmemory;
+
+namespace ACConsoleTest {
+    class Program {
+        static void Main(string[] args) {
+            AssettoCorsa ac = new AssettoCorsa();
+            ac.PhysicsInterval = 1000;
+            ac.PhysicsUpdated += ac_PhysicsInfoUpdated;
+            ac.Start();
+
+            Console.ReadKey();
+            ac.Stop();
+        }
+
+        static void ac_PhysicsInfoUpdated(object? sender, PhysicsEventArgs e) {
+            Console.WriteLine("PhysicsInfo");
+            Console.WriteLine("  Speed: " + e.Physics.SpeedKmh);
+            Console.WriteLine("  Roll: " + e.Physics.Roll);
+            Console.WriteLine("  Pitch: " + e.Physics.Pitch);
+            Console.WriteLine("  Heading: " + e.Physics.Heading);
+            float[] localangularvelocity = e.Physics.LocalAngularVelocity;
+            Console.Write("  LocalAngularVelocity: ");
+            foreach (float value in localangularvelocity) {
+                Console.Write(value);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ assettocorsasharedmemory/GameStatusEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assettocorsasharedmemory
+{
+    public class GameStatusEventArgs : EventArgs
+    {
+        public AC_STATUS GameStatus {get; private set;}
+
+        public GameStatusEventArgs(AC_STATUS status)
+        {
+            GameStatus = status;
+        }
+    }
+}
